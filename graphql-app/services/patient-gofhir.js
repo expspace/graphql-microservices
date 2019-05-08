cc8fec114cf4a0245a16bbad5f260f90544bcc2b
expspace <sebastian.shah@mcgill.ca>
2019-05-08 15:02:12 -0400

    initialize gofhir-ga4gh id map script

--- graphql-app/services/patient-gofhir.js
+++ graphql-app/services/patient-gofhir.js
@@ -1,15 +1,10 @@
 var rp = require('request-promise');
 var baseUrls = require('../config').BASE_SERVICE_URLS;
-var gofhirGA4GHIdMap = require('../gofhir-ga4gh-id-map').GOFHIR_ID_TO_GA4GH_CALLSET_ID_MAP;
 var paramToResourceMap = require('../constants').GQL_PARAM_TO_RESOURCE_MAP;
 
 export default {
     getPatient(args) {
         return rp(`${baseUrls.localGofhir}/Patient/${args.id}`)
-            .then(response => {
-                console.log(response);
-                return response
-            })
             .then(response => JSON.parse(response));
     },
 
@@ -20,18 +15,10 @@ export default {
 
         let patientList = rp(url)
             .then(response => JSON.parse(response))
-            .then(response => {console.log(response); return response} )
             .then(response => response.entry.map(patient => patient.resource))
-            // .then(filterPatientsWithIDMap);
 
         return patientList;
     }
-}
-
-//include only patients in patient list which have mappings from gofhir ID to ga4gh callset ID
-
-function filterPatientsWithIDMap(patients) {
-    return patients.filter(patient => Object.keys(gofhirGA4GHIdMap).includes(patient.id));
 }
 
 function prepQueryString(args) {
---
+++ scripts/initialize-id-map.js
@@ -0,0 +1,59 @@
+/* initialize-id-map.js */
+
+/* script which initializes gofhir-ga4gh-id-map.js
+   gofhir-ga4gh-id-map.js maps gofhir patient IDs to ga4gh callset IDs in order to link
+   patient records with variant information from separate systems*/
+
+const rp = require('../graphql-app/node_modules/request-promise');
+const fs = require('fs');
+const path = require('path');
+
+const idMapPath = path.join(__dirname, '../graphql-app/gofhir-ga4gh-id-map.js');
+
+//TODO use config, constants in /grapphql-app (babel transpile?)
+const LOCAL_GOFHIR_URL = 'http://localhost:3001';
+const LOCAL_GA4GH_URL = 'http://localhost:8000';
+const VARIANT_SET_ID = 'WyJ0ZXN0ZGF0YXNldCIsInZzIiwiZ2E0Z2gtZXhhbXBsZS1kYXRhIl0';
+
+// fetch gofhir patient id list
+let url = `${LOCAL_GOFHIR_URL}/Patient?_count=10000`;
+let patientIdList = rp(url)
+    .then(response => JSON.parse(response))
+    .then(response => response.entry.map(item => item.resource.id));
+
+// fetch ga4gh callset id list
+let options = {
+    method: "POST",
+    uri: `${LOCAL_GA4GH_URL}/callsets/search`,
+    body: {
+        variantSetId: VARIANT_SET_ID,
+    },
+    json: true,
+};
+
+let callsetIdList = rp(options)
+    .then(response => response.callSets.map(item => item.id));
+
+Promise.all([patientIdList, callsetIdList])
+    .then(values => createIdMap(values[0], values[1]))
+    .then(data => fs.writeFileSync(idMapPath, data));
+
+/**
+ * Creates a gofhir-ga4gh-id-map.js string
+ * @param {Object} options config options
+ */
+
+function createIdMap(patientIdList, callsetIdList) {
+    let string = "";
+
+    string = string.concat(`/** Constant used to map gofhir patient IDs to ga4gh callset IDs **/\n\n`);
+    string = string.concat(`export const GOFHIR_ID_TO_GA4GH_CALLSET_ID_MAP = {};\n\n`);
+
+    for(var i = 0; i< patientIdList.length; i++) {
+        string = string.concat(`GOFHIR_ID_TO_GA4GH_CALLSET_ID_MAP["${patientIdList[i]}"]`);
+        string = string.concat(' = ');
+        string = string.concat(`"${callsetIdList[i % callsetIdList.length]}";\n`);
+    }
+
+    return string;
+}
